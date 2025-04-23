3af5576d9dc49ee6af5de710c2a12b2219645901
Cameron Way <cameron.way@sap.com>
2025-04-22 17:41:46 -0700

    Fix mission control and aerospace

--- scripts/mac/install.sh
+++ scripts/mac/install.sh
@@ -4,6 +4,7 @@ defaults write -g ApplePressAndHoldEnabled -bool false
 defaults write -g InitialKeyRepeat -int 15
 defaults write -g KeyRepeat -int 1
 defaults write -g NSWindowShouldDragOnGesture -bool true
+defaults write com.apple.dock expose-group-apps -bool true # Fixes mission control with Aerospace
 
 sudo pmset repeat shutdown MTWRF 3:00:00
 
