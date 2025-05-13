63d5a7419abeb837d65c47c6b1d05bda8645aae7
Cameron Way <cameron.way@sap.com>
2025-05-13 11:52:28 -0700

    Update vscode settings

--- scripts/mac/symlinks.sh
+++ scripts/mac/symlinks.sh
@@ -2,5 +2,5 @@
 
 ln -sF $PWD/../../vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
 ln -sF $PWD/../../vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
-ln -sF $PWD/../../.lazygit_config.yml ~/Library/Application\ Support/lazygit/config.yml
+ln -sF $PWD/../../.lazygit_config.yml ~/.config/lazygit/config.yml
 ln -sF $PWD/.Brewfile ~/.Brewfile
