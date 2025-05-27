32d6ed231abbcfb598488e19644231a81938419e
Cameron Way <cameron.way@sap.com>
2025-05-26 17:41:40 -0700

    Fix cron

--- scripts/cron/cron.sh
+++ scripts/cron/cron.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-brew update
-brew upgrade
-brew cleanup
-brew cask cleanup
-brew bundle dump --global --force
+/opt/homebrew/bin/brew update
+/opt/homebrew/bin/brew upgrade
+/opt/homebrew/bin/brew cleanup
+/opt/homebrew/bin/brew cask cleanup
+/opt/homebrew/bin/brew bundle dump --global --force
