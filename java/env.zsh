ddd885d548165279e179014f7b2f2ef1a992f908
Cameron Way <cameron.way@sap.com>
2025-06-12 11:43:19 -0700

    Add back git.local example

--- java/env.zsh
+++
@@ -1,6 +0,0 @@
-if [ -z "$JAVA_VERSION" ]; then
-  export JAVA_VERSION=17
-fi
-
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/sapmachine-${JAVA_VERSION}.jdk/Contents/Home
-export MAVEN_VERSION=3.8.8
