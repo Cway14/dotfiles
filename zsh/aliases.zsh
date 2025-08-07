90a00826959c323a68da6801047e983c7b9a848f
Cameron Way <cameron.way@sap.com>
2025-08-07 09:11:22 -0700

    OMZ aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -12,3 +12,40 @@ alias so="source ~/.zshrc"
 alias dev="cd ~/dev"
 alias dots="cd $ZSH"
 alias config="cd $XDG_CONFIG_HOME"
+alias zshrc='${=EDITOR} ${ZDOTDIR:-$HOME}/.zshrc' # Quick access to the .zshrc file
+
+alias grep='grep --color'
+alias sgrep='grep -R -n -H -C 5 --exclude-dir={.git,.svn,CVS} '
+
+alias lsa='ls -lah'
+alias ll='ls -lh'
+alias la='ls -lAh'
+
+alias -g ...='../..'
+alias -g ....='../../..'
+alias -g .....='../../../..'
+alias -g ......='../../../../..'
+
+alias -- -='cd -'
+alias 1='cd -1'
+alias 2='cd -2'
+alias 3='cd -3'
+alias 4='cd -4'
+alias 5='cd -5'
+alias 6='cd -6'
+alias 7='cd -7'
+alias 8='cd -8'
+alias 9='cd -9'
+
+alias t='tail -f'
+
+# Command line head / tail shortcuts
+alias -g H='| head'
+alias -g T='| tail'
+alias -g G='| grep'
+alias -g L="| less"
+alias -g M="| most"
+alias -g LL="2>&1 | less"
+alias -g CA="2>&1 | cat -A"
+alias -g NE="2> /dev/null"
+alias -g NUL="> /dev/null 2>&1"
