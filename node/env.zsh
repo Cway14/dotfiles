7d73a57b13e947195f9747fbe45a973c733d8462
Cameron Way <cameron.way@sap.com>
2025-08-19 10:03:20 -0700

    Remove nvm

--- node/env.zsh
+++ node/env.zsh
@@ -2,7 +2,4 @@ export NODE_VERSION=22.12.0
 export NODE_COMPILE_CACHE=.cache/nodejs-compile-cache
 export NODE_OPTIONS=--max_old_space_size=15360
 export TSC_WATCHFILE=UseDynamicPriorityPolling
-
-export NVM_DIR="$HOME/.nvm"
-  [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
-  [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"
+export N_PREFIX=$HOME/.local
